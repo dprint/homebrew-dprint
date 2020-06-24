55ba0d44ff15e96c3be034346ea5bf6492f53cf6
David Sherret <dsherret@gmail.com>
2020-06-24 12:49:45 -0400

    Fix update-formula.sh

--- scripts/update-formula.sh
+++ scripts/update-formula.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 version=$1
 url=`echo "https://github.com/dprint/dprint/releases/download/VERSION/dprint-x86_64-apple-darwin.tar.gz" | sed -e "s/VERSION/$version/g"`
-sha=`curl $url &> /dev/stdout | shasum -a 256 | cut -d' ' -f1`
+sha=`curl -Ls $url 2>&1 | shasum -a 256 | cut -d' ' -f1`
 cat scripts/assets/dprint-formula.txt | sed -e "s/SHA/$sha/g" -e "s/VERSION/$version/g" > Formula/dprint.rb
