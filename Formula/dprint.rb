b0b9a328b100198421d76c38ae6d0e31b81b0bea
David Sherret <dsherret@users.noreply.github.com>
2020-07-20 23:54:23 -0400

    Update because the tar.gz was wrong.

--- Formula/dprint.rb
+++ Formula/dprint.rb
@@ -2,7 +2,7 @@ class Dprint < Formula
     desc "Pluggable and configurable code formatting platform."
     homepage "https://github.com/dprint/dprint"
     url "https://github.com/dprint/dprint/releases/download/0.6.3/dprint-x86_64-apple-darwin.tar.gz"
-    sha256 "a1b5ed25f94094b79926b88c6c6bd1704df672e75f004901c895843562d886e6"
+    sha256 "0fdd6e459812d7df544b5c27b1803974ffd8faa860ccb81c9e2d006615ff6abd"
     version "0.6.3"
 
     def install
