c9816a4d3adedf42aaaffb421335cc1edd72edd6
David Sherret <dsherret@users.noreply.github.com>
2020-08-03 23:29:14 +0000

    0.7.0

--- Formula/dprint.rb
+++ Formula/dprint.rb
@@ -2,7 +2,7 @@ class Dprint < Formula
     desc "Pluggable and configurable code formatting platform."
     homepage "https://github.com/dprint/dprint"
     url "https://github.com/dprint/dprint/releases/download/0.7.0/dprint-x86_64-apple-darwin.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "ec2985febe0638e6f58075eb6a22825f1a47b8be842c9c464d8d000787e88be2"
     version "0.7.0"
 
     def install
